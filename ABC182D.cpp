c16ae675201017a1875f8f21e28bcb9c16fe70b8
fumiy <fumiy@uchicago.edu>
2021-02-19 13:09:25 -0600

    EducationalDPContest-K

--- ABC182D.cpp
+++ ABC182D.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #define rep(i,n)for(int i=0;i<(int)(n);i++)
 using namespace std;
-using Graph=vector<vector<int>>;
+using Graph=vector<vector<int> >;
 using p=pair<long,long>;
 const int64_t mod = 1000000007;
 
