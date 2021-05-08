21788843a460c337c6ab5ce1597964c0e777c019
Fumi Yoshikoshi <fumiy@cs.uchicago.edu>
2021-05-08 17:26:13 -0500

    05 08 2021

---
+++ LeetCode/Algorithms/9-Palindrome Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        boo = True
+        sx = str(x)
+        l = len(sx)
+        for i in range(0,l):
+            if str(x)[i] != str(x)[l-i-1]:
+                boo = False
+                return boo
+        return boo
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
